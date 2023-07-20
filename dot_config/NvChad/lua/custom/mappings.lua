ba9aa8e2f505fb0ff4f816da3931b5c31f3e56bb
akladnig <adrian@kladnig.com>
2023-07-21 08:20:44 +1000

    added visidata

--- dot_config/NvChad/lua/custom/mappings.lua
+++ dot_config/NvChad/lua/custom/mappings.lua
@@ -8,16 +8,16 @@ M.general = {
     ["<C-d>"] = { "<C-d>zz", "Half page down and re-centre" },
     ["<C-u>"] = { "<C-u>zz", "Half page up and re-centre" },
     -- Move Lines ∆ is <A-j> and ˚ is <A-k>
-    ["<A-j>"] = { "<cmd>m .+1<cr>==", "Move down" },
-    ["<A-k>"] = { "<cmd>m .-2<cr>==", "Move up" },
+    ["<A-j>"] = { "<cmd>m .+1<cr>==", "Move line down" },
+    ["<A-k>"] = { "<cmd>m .-2<cr>==", "Move line up" },
   },
   i = {
-    ["<A-j>"] = { "<esc><cmd>m .+1<cr>==gi", "Move down" },
-    ["<A-k>"] = { "<esc><cmd>m .-2<cr>==gi", "Move up" },
+    ["<A-j>"] = { "<esc><cmd>m .+1<cr>==gi", "Move line down" },
+    ["<A-k>"] = { "<esc><cmd>m .-2<cr>==gi", "Move line up" },
   },
   v = {
-    ["<A-j>"] = { ":m '>+1<cr>gv=gv", "Move down" },
-    ["<A-k>"] = { ":m '<-2<cr>gv=gv", "Move up" },
+    ["<A-j>"] = { ":m '>+1<cr>gv=gv", "Move line down" },
+    ["<A-k>"] = { ":m '<-2<cr>gv=gv", "Move line up" },
   },
 }
 
--- dot_config/NvChad/lua/custom/plugins.lua
+++ dot_config/NvChad/lua/custom/plugins.lua
@@ -68,14 +68,25 @@ local plugins = {
   },
   -- Install a plugin
   {
-    "ggandor/leap.nvim",
-    name = "leap",
-    lazy = false,
-    config = function()
-      require("leap").add_default_mappings()
-    end,
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts =  {
+      modes = {
+        char = {
+          jump_labels = false,
+        },
+      },
+     },
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
-
   -- lazy.nvim
   {
     "folke/noice.nvim",
