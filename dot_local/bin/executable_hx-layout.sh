7644072b26f4e3e2d94639260f61e15824775c02
akladnig <adrian@kladnig.com>
2023-08-17 21:44:22 +1000

    Added script to open helix in IDE mode

--- dot_local/bin/executable_hx-layout.sh
+++ dot_local/bin/executable_hx-layout.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env sh
 
-# get the current pane id.
-hx_pane=$(wezterm cli activate-pane)
-lf_pane=$(wezterm cli split-pane --left --cells 60)
-echo "lf -config ~/.config/lf/lfhxrc" | wezterm cli send-text --pane-id $lf_pane --no-paste
-t_pane=$(wezterm cli split-pane --bottom --cells 1)
-wezterm cli activate-pane-direction up
-echo "hx ." | wezterm cli send-text --no-paste
-wezterm cli activate-pane-direction left
+fpath="$1"
 
+# Get the pane id
+hx_pane=$WEZTERM_PANE
+echo "hx pane"
+  echo $hx_pane
+explorer_pane=$(wezterm cli split-pane --left --cells 1)
+echo "lf -config ~/.config/lf/lfhxrc" | wezterm cli send-text --pane-id $explorer_pane --no-paste
+terminal_pane=$(wezterm cli split-pane --bottom --cells 1)
+
+# open the file in helix or open the explorer
+if [ -z "${fpath}" ]; then
+  echo "hx" | wezterm cli send-text --pane-id $hx_pane --no-paste
+  wezterm cli adjust-pane-size --pane-id $explorer_pane --amount 60 Right
+  wezterm cli activate-pane --pane-id $explorer_pane
+else
+  echo "hx ${fpath}" | wezterm cli send-text --pane-id $hx_pane --no-paste
+  wezterm cli activate-pane --pane-id $hx_pane
+fi
