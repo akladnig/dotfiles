116a73d15862313828e04aeb2d266eac05572ba6
akladnig <adrian@kladnig.com>
2023-08-20 16:29:09 +1000

    update time format

--- dot_config/zsh/dot_p10k.zsh
+++ dot_config/zsh/dot_p10k.zsh
@@ -1688,8 +1688,8 @@
 
   ####################################[ time: current time ]####################################
   # Current time color.
-  typeset -g POWERLEVEL9K_TIME_FOREGROUND=0
-  typeset -g POWERLEVEL9K_TIME_BACKGROUND=7
+  typeset -g POWERLEVEL9K_TIME_FOREGROUND=254
+  typeset -g POWERLEVEL9K_TIME_BACKGROUND=99
   # Format for the current time: 09:51:02. See `man 3 strftime`.
   typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%I:%M:%S %p}'
   # If set to true, time will update when you hit enter. This way prompts for the past
